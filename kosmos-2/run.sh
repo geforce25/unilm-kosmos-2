88d7c55871eb07bc4cf1f9943f4f5ee64766a406
Gabe Nataneli <gabrielenataneli@yahoo.com>
2023-07-06 01:13:01 -0700

    Run script publishes GRADIO port * the run script now correclty publishes the port required by gradio to host the sevice * more tweaks to the Docker file to correct issues CUDA not being found by the xformers library

--- kosmos-2/run.sh
+++ kosmos-2/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --gpus=all --ipc=host --privileged --name kosmos-2-container -v kosmos-2:/home/ai/models kosmos-2 bash
+docker run -it --rm --gpus=all --ipc=host --privileged --publish 7860:7860 --name kosmos-2-container -v kosmos-2:/home/ai/models kosmos-2 bash
